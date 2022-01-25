72e8fe975e50c6e2a01c98e3b26168ee62f4f4d6
Manan Luthra <mananluthra123@gmail.com>
2022-01-25 16:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 $(window).on('load', function() {
   $(".preloader-1").fadeOut(100)
 });
+
+$(window).resize(function(){
+        if(window.innerWidth < 1200) {
+            $("iframe").remove(".phone, .ethereum");
+
+        }
+});
